2e78ad2ea2c00d868f517452e95846b830c22d5d
Zoraiz Sibtain <zoraizsybtain@gmail.com>
2026-04-07 00:16:17 +0000

    remove speed-insights

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
